9f35c090c2eab678855a6c136854c5d251117e87
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-10-02 23:15:54 +0100

    deploying to render

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectionMethod = async () => {
     try {
         const conn = await mongoose.connect("mongodb+srv://Franklin:emmaofmcap%407@cluster0.jmchgoz.mongodb.net/?retryWrites=true&w=majority")
-        console.log(`Db connected: ${conn.connection.host}`)
+        console.log("Db connected successfully")
     } catch (error) {
         console.log(error)
         process.exit(1)

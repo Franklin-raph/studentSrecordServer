b0a232b2bd600a944701fe9e2515110a40f93cd8
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-10-02 23:28:09 +0100

    deploying to render

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionMethod = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://Franklin:emmaofmcap%407@cluster0.jmchgoz.mongodb.net/?retryWrites=true&w=majority")
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("Db connected successfully")
     } catch (error) {
         console.log(error)
